dylanneve1/EOL_hardware_samsung_slsi

graphics: libhwjpeg: legacy support for ExynosJpegEncoderForCamera
* Matching proprietary libhwjpeg from Android 10 stock ROM.
* ExynosJpegEncoderForCamera takes only one arg as bBTBComp.

Adapted from Linux4's commit to bring-up hwjpeg for A11+ support on Q based BSP.

Co-Authored-By: Tim Zimmermann <43107815+Linux4@users.noreply.github.com>

graphics/base/libhwjpeg/ExynosJpegEncoderForCamera.cpp
@@ -67,8 +67,14 @@ static int GetThumbnailFormat(int v4l2Format)
             return v4l2Format;
 }
 
+#ifdef USES_LEGACY_HWJPEG
+#define HWJPEG_INDEX 0
+ExynosJpegEncoderForCamera::ExynosJpegEncoderForCamera(bool bBTBComp)
+        : ExynosJpegEncoder(HWJPEG_INDEX),
+#else
 ExynosJpegEncoderForCamera::ExynosJpegEncoderForCamera(bool bBTBComp, unsigned int index)
         : ExynosJpegEncoder(index),
+#endif
           m_phwjpeg4thumb(NULL), m_fdIONClient(-1), m_fdIONThumbImgBuffer(-1), m_pIONThumbImgBuffer(NULL),
           m_szIONThumbImgBuffer(0), m_pIONThumbJpegBuffer(NULL), m_fdIONThumbJpegBuffer(-1), m_szIONThumbJpegBuffer(0),
           m_nThumbWidth(0), m_nThumbHeight(0), m_nThumbQuality(0),
@@ -80,7 +86,11 @@ ExynosJpegEncoderForCamera::ExynosJpegEncoderForCamera(bool bBTBComp, unsigned i
         return;
     }
 
+#ifdef USES_LEGACY_HWJPEG
+    m_phwjpeg4thumb = new CHWJpegV4L2Compressor(jpeg_node[HWJPEG_INDEX]);
+#else
     m_phwjpeg4thumb = new CHWJpegV4L2Compressor(jpeg_node[index]);
+#endif
     if (!m_phwjpeg4thumb) {
         ALOGE("Failed to create thumbnail compressor!");
         return;

graphics/base/libhwjpeg/include/ExynosJpegEncoderForCamera.h
@@ -100,7 +100,11 @@ class ExynosJpegEncoderForCamera: public ExynosJpegEncoder {
 protected:
     virtual bool EnsureFormatIsApplied();
 public:
+#ifdef USES_LEGACY_HWJPEG
+    ExynosJpegEncoderForCamera(bool bBTBComp = true);
+#else
     ExynosJpegEncoderForCamera(bool bBTBComp = true, unsigned int index = 0);
+#endif
     virtual ~ExynosJpegEncoderForCamera();
 
     int encode(int *size, exif_attribute_t *exifInfo, char** pcJpegBuffer, debug_attribute_t *debugInfo = 0);

ouroboros420/hardware_samsung_slsi

graphics/base/libhwjpeg/ExynosJpegEncoderForCamera.cpp
@@ -67,8 +67,14 @@ static int GetThumbnailFormat(int v4l2Format)
             return v4l2Format;
 }
 
+#ifdef USE_LEGACY_HWJPEG
+#define HWJPEG_INDEX 0
+ExynosJpegEncoderForCamera::ExynosJpegEncoderForCamera(bool bBTBComp)
+        : ExynosJpegEncoder(HWJPEG_INDEX),
+#else
 ExynosJpegEncoderForCamera::ExynosJpegEncoderForCamera(bool bBTBComp, unsigned int index)
         : ExynosJpegEncoder(index),
+#endif
           m_phwjpeg4thumb(NULL), m_fdIONClient(-1), m_fdIONThumbImgBuffer(-1), m_pIONThumbImgBuffer(NULL),
           m_szIONThumbImgBuffer(0), m_pIONThumbJpegBuffer(NULL), m_fdIONThumbJpegBuffer(-1), m_szIONThumbJpegBuffer(0),
           m_nThumbWidth(0), m_nThumbHeight(0), m_nThumbQuality(0),
@@ -80,7 +86,11 @@ ExynosJpegEncoderForCamera::ExynosJpegEncoderForCamera(bool bBTBComp, unsigned i
         return;
     }
 
+#ifdef USE_LEGACY_HWJPEG
+    m_phwjpeg4thumb = new CHWJpegV4L2Compressor(jpeg_node[HWJPEG_INDEX]);
+#else
     m_phwjpeg4thumb = new CHWJpegV4L2Compressor(jpeg_node[index]);
+#endif
     if (!m_phwjpeg4thumb) {
         ALOGE("Failed to create thumbnail compressor!");
         return;

graphics/base/libhwjpeg/include/ExynosJpegEncoderForCamera.h
@@ -100,7 +100,11 @@ class ExynosJpegEncoderForCamera: public ExynosJpegEncoder {
 protected:
     virtual bool EnsureFormatIsApplied();
 public:
+#ifdef USE_LEGACY_HWJPEG
+    ExynosJpegEncoderForCamera(bool bBTBComp = true);
+#else
     ExynosJpegEncoderForCamera(bool bBTBComp = true, unsigned int index = 0);
+#endif
     virtual ~ExynosJpegEncoderForCamera();
 
     int encode(int *size, exif_attribute_t *exifInfo, char** pcJpegBuffer, debug_attribute_t *debugInfo = 0);